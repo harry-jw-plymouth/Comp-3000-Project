5eb68dc5f86c772bbeabf8dbd96c6b8bfb70af65
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2025-11-12 17:44:10 +0000

    Addition/change: Made script for creating base grid (previously hard coded/placed)

---
+++ Code/GameFiles/Assets/Scripts/GridCreator.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GridCreator : MonoBehaviour
+{
+    public const int WIDTH = 100;
+    public const int HEIGHT = 100;
+    public Tilemap GameMap;
+    public RuleTile GameTile;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void CreateGrid()
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            for (int y = 0; y < HEIGHT; y++)
+            {
+                Vector3Int CurrentPosition=new Vector3Int(x, y, 0);
+                GameMap.SetTile(CurrentPosition, GameTile);
+            }
+        }
+    }
+}
