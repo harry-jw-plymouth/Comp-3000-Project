185f9972db3390cff90860f79d6e4f09d2f198ea
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2025-11-14 16:10:12 +0000

    Addition: added road placing mode: Will add more materials to add/ place in future

--- Code/GameFiles/Assets/Scripts/GridCreator.cs
+++ Code/GameFiles/Assets/Scripts/GridCreator.cs
@@ -7,8 +7,10 @@ public class GridCreator : MonoBehaviour
     public const int WIDTH = 100;
     public const int HEIGHT = 100;
     public Tilemap GameMap;
+    bool UpdateNeeded = false;
 
     public RuleTile GameTile;
+    public RuleTile RoadTile;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,6 +20,39 @@ public class GridCreator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 ClickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int CellClickedPos = GameMap.WorldToCell(ClickPos);
+            Debug.Log("Click at: " + ClickPos);
+            Debug.Log("Click at: " + CellClickedPos);
+            if (UIHandlerScript.TileEditorOn == true)
+            {
+                try
+                {
+                    if (GameGrid[CellClickedPos.x, CellClickedPos.y].Contains == 0)
+                    {
+                        GameGrid[CellClickedPos.x, CellClickedPos.y].Contains = 1;
+                        GameMap.SetTile(CellClickedPos, RoadTile);
+                    }
+                    else
+                    {
+                        GameGrid[CellClickedPos.x, CellClickedPos.y].Contains = 0;
+                        GameMap.SetTile(CellClickedPos, GameTile);
+
+                    }
+                }
+                catch
+                {
+                    Debug.Log("Click not in grid square");
+
+                }
+                
+                 
+
+               
+            }
+        }
         
     }
     void CreateGrid()
--- Code/GameFiles/Assets/Scripts/UIHandlerScript.cs
+++ Code/GameFiles/Assets/Scripts/UIHandlerScript.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 
 public class UIHandlerScript : MonoBehaviour
 {
+    public static bool TileEditorOn;
     //  public Square[,] GameGrid;
     private void Start()
     {
-        //  GameGrid = new Square[GridCreator.WIDTH, GridCreator.HEIGHT];
-        //   SetGrid();
-    }
+        TileEditorOn = false ;
+    //  GameGrid = new Square[GridCreator.WIDTH, GridCreator.HEIGHT];
+    //   SetGrid();
+}
     public void OnRoadButtonClicked()
     {
         Debug.Log("Road button clicked");
+        if (TileEditorOn)
+        {
+            TileEditorOn = false;
+        }
+        else
+        {
+            TileEditorOn= true;
+        }
     }
 }
