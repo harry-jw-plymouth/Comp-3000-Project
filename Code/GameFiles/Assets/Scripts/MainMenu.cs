82c9b96b750d46c3f2d4048c2b7999a6059152d9
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2025-12-05 14:46:31 +0000

    Addition: added button to main menu

--- Code/GameFiles/Assets/Scripts/MainMenu.cs
+++ Code/GameFiles/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject SavesCanvas; //scene 1
+    public GameObject StartButton; // scene 0
+    int CurrentScene = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
     public void OnStartClicked()
+    {
+        StartButton.SetActive(false);
+        SavesCanvas.SetActive(true);
+
+        
+    }
+    public void OnGenerateNewClicked()
     {
         SceneManager.LoadScene("GameScene");
     }
