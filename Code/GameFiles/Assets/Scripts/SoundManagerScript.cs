ceb983e2addf0c5eeb9ad7f484f3c52a1788200d
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2026-02-06 15:49:38 +0000

    Added background ost

--- Code/GameFiles/Assets/Scripts/SoundManagerScript.cs
+++ Code/GameFiles/Assets/Scripts/SoundManagerScript.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class SoundManagerScript : MonoBehaviour
 {
+    AudioSource BackgroundOST;
+
+    private void Awake()
+    {
+        BackgroundOST = GetComponent<AudioSource>();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
+        BackgroundOST.Play();
     }
 
     // Update is called once per frame
