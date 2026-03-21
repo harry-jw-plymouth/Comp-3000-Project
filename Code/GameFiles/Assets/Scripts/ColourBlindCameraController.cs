54fd30e30cac30238f72d70db22f4aaae24958e0
Harry-JW <146178087+Harry-JW@users.noreply.github.com>
2026-03-21 21:16:02 +0000

    Added player preferences to save selected colour blind mode

--- Code/GameFiles/Assets/Scripts/ColourBlindCameraController.cs
+++ Code/GameFiles/Assets/Scripts/ColourBlindCameraController.cs
@@ -9,7 +9,7 @@ public class ColourBlindCameraController : MonoBehaviour
     {
         Normal=0,
         Protanopia=1,
-        Deuteranopia2,
+        Deuteranopia=2,
         Tritanopia=3
     }
 
@@ -30,13 +30,30 @@ public class ColourBlindCameraController : MonoBehaviour
     public void SetMode(int NewMode)
     {
         mode = (ColourBlindMode)NewMode;
+        SavePreference();
         Debug.Log("Colour Blind Mode set to: " + mode.ToString());
+    }
+    public void LoadPreference()
+    {
+        if (PlayerPrefs.HasKey("ColourBlindMode"))
+        {
+            mode = (ColourBlindMode)PlayerPrefs.GetInt("ColourBlindMode");
+        }
+        else
+        {
+            mode = ColourBlindMode.Normal;
+        }
+    }
+    public void SavePreference()
+    {
+        PlayerPrefs.SetInt("ColourBlindMode", (int)mode);
+        PlayerPrefs.Save();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        LoadPreference();
     }
 
     // Update is called once per frame
